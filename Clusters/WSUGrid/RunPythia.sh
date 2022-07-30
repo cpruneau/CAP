48efb3865cd6b91f4d3df4f3825f50f4413769d5
Claude Andre Pruneau <aa7526@warrior.nas.grid.wayne.edu>
2022-07-30 14:23:04 -0400

    Adding RunGridAnalysis.C

--- Clusters/WSUGrid/RunPythia.sh
+++ Clusters/WSUGrid/RunPythia.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-
-####################################################################################################
-echo "Setting up WAC"
-####################################################################################################
-source /home/victorG/PROJECTS/ROOT/root_install/bin/thisroot.sh
-export PYTHIA8=/home/victorG/PROJECTS/PYTHIA8/pythia8307
-export WAC_ROOT=/home/victorG/PROJECTS/WAC
-export WAC_SOURCE="$WAC_ROOT"
-export WAC_BIN="$WAC_ROOT/bin"
-export WAC_LIB="$WAC_ROOT/lib"
-
-export PATH="$WAC_BIN:$PATH"
-export DYLD_LIBRARY_PATH="$WAC_LIB:$PYTHIA8/lib:$DYLD_LIBRARY_PATH"
-export LD_LIBRARY_PATH="$WAC_LIB:$PYTHIA8/lib:$LD_LIBRARY_PATH"
-
-TASKIX=$SLURM_ARRAY_TASK_ID
+conda init bash
+conda activate CProot
+echo `which root` 
+export CAP_ROOT="/wsu/home/aa/aa75/aa7526/CAP/"
+export CAP_SRC="$CAP_ROOT/src/"
+export CAP_BIN="$CAP_ROOT/bin/"
+export CAP_LIB="$CAP_ROOT/lib/"
+export CAP_DATA="$CAP_ROOT/data/"
+export PATH="$CAP_BIN:$PATH"
+export DYLD_LIBRARY_PATH="$CAP_LIB:$DYLD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$CAP_LIB:$LD_LIBRARY_PATH"
+export CAP_INPUT_PATH="$CAP_DATA/InputFiles/"
+#export CAP_OUTPUT_PATH="$CAP_DATA/OutputFiles/"
+echo "Environment variables are now defined for CAP"
+module load  pythia
+TASKIX=1 #$SLURM_ARRAY_TASK_ID
 SEED=$(( SLURM_ARRAY_TASK_ID + SLURM_ARRAY_JOB_ID*1000 ))
+echo "========================================================================================"
+echo "Calling root w/ RunAnalysis"
+echo "========================================================================================"
+root -b "/wsu/home/aa/aa75/aa7526/CAP/src/Macros/RunGridAnalysis.C($TASKIX,$SEED)"  
 
-# Execute application code
-RunPythiaSimulationTwoParticlesDiff $TASKIX $SEED
 
